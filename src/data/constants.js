9ec71ab8edd699786489318813e53edd9e2ffa37
Shambhavi Anand <125148718+shambhavi-anand01@users.noreply.github.com>
2024-08-18 17:17:53 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/shambhavi-anand01",
     resume:
-      "https://drive.google.com/file/d/1aNgsFT24KVycX_DB5bA8S9KQt4KcS_Pz/view?usp=drive_link",
+      "https://drive.google.com/file/d/1IXHBaTVhDpGUwarIygqFMdEHhqd0hYS4/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/shambhavi-anand-520391187",
   };
   
